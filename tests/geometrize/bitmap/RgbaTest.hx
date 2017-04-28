00005ff323f6359c69d2287c99a9db01fa4d24a4
sam <sam@silver42.karoo.co.uk>
2017-04-28 02:28:41 +0100

    Remove redundant tests, update submodule

--- tests/geometrize/bitmap/RgbaTest.hx
+++ tests/geometrize/bitmap/RgbaTest.hx
@@ -28,26 +28,6 @@ class RgbaTest {
 		Assert.equals(Rgba.create(0, 255, 0, 255), 0x00FF00FF);
 	}
 	
-	public function testAddRgba() {
-		Assert.equals(0xFFFFFFFF, Rgba.create(128, 128, 128, 128) + Rgba.create(127, 127, 127, 127));
-		Assert.equals(0xFF00FF00, Rgba.create(255, 0, 0, 0) + Rgba.create(0, 0, 255, 0));
-		
-		Assert.equals(0xFFFFFFFF, Rgba.create(255, 255, 255, 255) + Rgba.create(255, 255, 255, 255) + Rgba.create(255, 255, 255, 255));
-		Assert.equals(0xFFFFFFFF, Rgba.create(200, 200, 200, 200) + Rgba.create(300, 300, 300, 300));
-		
-		Assert.equals(0x10101010, Rgba.create(8, 8, 8, 8) + Rgba.create(4, 4, 4, 4) + Rgba.create(4, 4, 4, 4));
-	}
-	
-	public function testSubtractRgba() {
-		Assert.equals(0x00000000, Rgba.create(255, 255, 255, 255) - Rgba.create(255, 255, 255, 255) - Rgba.create(255, 255, 255, 255));
-		Assert.equals(0x00FF00FF, Rgba.create(255, 255, 255, 255) - Rgba.create(255, 0, 255, 0));
-		
-		Assert.equals(0x00000000, Rgba.create(128, 128, 128, 128) - Rgba.create(128, 128, 128, 128));
-		Assert.equals(0x00000000, Rgba.create(50, 50, 50, 50) - Rgba.create(100, 100, 100, 100));
-		
-		Assert.equals(0x10101010, Rgba.create(32, 32, 32, 32) - Rgba.create(16, 16, 16, 16));
-	}
-	
 	public function testAccessors() {
 		var color = Rgba.create(255, 127, 63, 31);
 		Assert.equals(color.r, 255);
